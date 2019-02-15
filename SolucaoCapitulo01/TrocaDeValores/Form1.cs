4bb1bbe8d18d1410a7afb1613441fcd776f7cec0
David Fernandes <david@softium.com.br>
2019-02-15 17:52:09 -0300

    init commit

---
+++ SolucaoCapitulo01/TrocaDeValores/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrocaDeValores
+{
+    public partial class frmTracaDeValores : Form
+    {
+        public frmTracaDeValores()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string auxiliar = txtPrimeiroValor.Text;
+            txtPrimeiroValor.Text = txtSegundoValor.Text;
+            txtSegundoValor.Text = auxiliar;
+            MessageBox.Show("Troca de valores concluída", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
